8543fd84f838a81acbcbd3ac99d4b9b674d534da
surbzia <101662816+surbzia@users.noreply.github.com>
2022-07-20 04:03:01 +0500

    init

---
+++ resources/js/Pages/Page2.vue
@@ -0,0 +1,13 @@
+<template>
+  <Link :href="route('page.page1')">Page 1</Link>
+  <Link :href="route('page.page2')">Page 2</Link>
+  <div>Welcome Page 2</div>
+</template>
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+export default {
+  components: {
+    Link,
+  },
+};
+</script>
